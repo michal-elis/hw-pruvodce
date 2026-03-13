f8753ea965dd4ce358f4536229ad0215bffc2f05
Michal Elis <m_elis@utb.cz>
2026-03-13 12:46:30 +0100

    opravy js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,5 +1,19 @@
 async function includeHTML(id, file) {
 
+    document.addEventListener("DOMContentLoaded", () => {
+
+  const current = location.pathname.split("/").pop();
+
+  document.querySelectorAll(".site-nav a").forEach(link => {
+
+    if(link.getAttribute("href") === current){
+      link.setAttribute("aria-current","page");
+    }
+
+  });
+
+});
+
   const target = document.getElementById(id);
   if (!target) return;
 
